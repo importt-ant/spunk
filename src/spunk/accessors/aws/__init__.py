ed9f01360ded5d17794edf3927e3520c241dbd1e
importt-ant <demon.and.max@gmail.com>
2026-03-14 18:43:29 +0200

    Rework ses access

--- src/spunk/accessors/aws/__init__.py
+++ src/spunk/accessors/aws/__init__.py
@@ -1,9 +1,9 @@
 from .dynamodb_table import DynamoDBTable
 from .s3_bucket import S3Bucket
-from .ses import SES
+from .ses_identity import SESIdentity
 
 __all__ = [
-    "DynamoDBTable", 
-    "S3Bucket", 
-    "SES"
+    "DynamoDBTable",
+    "S3Bucket",
+    "SESIdentity",
 ]
